e60ff1563e9f5ca30667ea3cac6a733e31a5d0e4
jeysonalvesc12 <jeyson.alves@aluno.ifsp.edu.br>
2026-07-28 21:48:41 -0300

    Update user information in hello_world function

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Aluno:</b></td><td>Jeyson</td></tr><tr><td><b>Prontuário:</b></td><td>PT3037126</td></tr></table>'
 
 @app.route('/user/<name>')
 def user(name):
